92fa00c1bd349efde0b202566e467b6c8610e053
ShinJiHye <werytuiop00@gmail.com>
2024-03-06 17:44:40 +0900

    CandyProject

---
+++ Assets/Scripts/Scene/MapScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScene : BaseScene
+{
+    public override IEnumerator LoadingRoutine()
+    {
+        yield return null;
+    }
+
+    public void GameSceneLoad()
+    {
+        Manager.Scene.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/Scene/TitleScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScene : BaseScene
+{
+    public override IEnumerator LoadingRoutine()
+    {
+        yield return null;
+    }
+
+    public void MapSceneLoad()
+    {
+        Manager.Scene.LoadScene("MapScene");
+    }
+}
